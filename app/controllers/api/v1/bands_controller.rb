1a527ea9b60fb2587106c59b2adda5015f3f9f50
James Young <jpblicer@gmail.com>
2024-09-08 20:04:08 +0900

    setup create action for bands controller

--- app/controllers/api/v1/bands_controller.rb
+++ app/controllers/api/v1/bands_controller.rb
@@ -7,4 +7,19 @@ class Api::V1::BandsController < ApplicationController
     end
     render json: @bands
   end
+
+  def create
+    @band = Band.new(band_params)
+    if @band.save
+      render json: @band, status: :created
+    else
+      render json: { error: @band.errors.messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:name, :country, members: {}, albums: [])
+  end
 end
