5c5b86cc5238df46f78055e9d22f0d89895ebe1d
James Young <jpblicer@gmail.com>
2024-09-08 19:09:45 +0900

    add conditional for params on band name

--- app/controllers/api/v1/bands_controller.rb
+++ app/controllers/api/v1/bands_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::BandsController < ApplicationController
   def index
-    @bands = Band.all
+    if params[:name].present?
+      @bands = Band.where('name ILIKE ?', "%#{params[:name]}%")
+    else
+      @bands = Band.all
+    end
     render json: @bands
   end
 end
